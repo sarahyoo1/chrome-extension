9715ade21e87783c2d049664377887bb2f7bcf8f
Sarah Yoo(유가은) <gaeunyoo3626@gmail.com>
2024-10-14 13:55:54 -0400

    firebase auth

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,8 +1,11 @@
+import { auth } from "@src/libs/firebase";
+import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
+
 
 console.log('background script loaded');
 
-export async function get_current_tab_id () : Promise<number|undefined> {
-  return new Promise((resolve) => {
+export async function get_current_tab_id () {
+  return new Promise<number|undefined>((resolve) => {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
         if (tabs.length == 0) {
             resolve(undefined);
@@ -21,3 +24,28 @@ export function open_as_side_panel() {
 }
 
 chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: true});
+
+export function getGoogleAuthCredential() {
+
+  return new Promise<ReturnType<typeof GoogleAuthProvider.credential>>((resolve, reject) => {
+    chrome.identity.getAuthToken({interactive: true}, (token) => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+        reject(chrome.runtime.lastError);
+      }
+      const credential = GoogleAuthProvider.credential(null, token);
+      resolve(credential);
+    });
+  });
+}
+
+export async function signIn() {
+  try {
+    const credential = await getGoogleAuthCredential();
+    const result = await signInWithCredential(auth, credential);
+    return result.user;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
--- src/pages/panel/Panel.tsx
+++ src/pages/panel/Panel.tsx
@@ -1,11 +1,35 @@
 import '@pages/panel/Panel.css';
 import { ChatList } from './ChatList';
+import { useState } from 'react';
+import { User } from "firebase/auth";
+import { auth } from '../../libs/firebase';
+import { signIn } from '../background';
 
 export default function Panel(): JSX.Element {
+const [user, setUser] = useState<User | null>();
+
+  const logIn = async () => {
+    const user = await signIn();
+    setUser(user);
+  }
+
+  const signOut = () => {
+    auth.signOut().then(() => {
+      setUser(null);
+    })
+  }
 
   return (
     <div className="flex flex-col h-full">
       <ChatList />
+      {user ? (
+        <>
+          <p className='text-white'>{user.email}</p>
+          <button onClick={signOut} className='btn'>Log Out</button>
+        </>
+      ) : (
+        <button onClick={logIn} className='btn'>Sign In</button>
+      )}
     </div>
   );
 }
